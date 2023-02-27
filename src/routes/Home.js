cb6e1e109b482535ddf132745076733cbb8a0c90
fran2bng <francoram13@gmail.com>
2023-02-27 01:45:52 -0300

    hola

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,11 +3,12 @@ import Navbar from "../components/Navbar";
 import ColorSchemesExample from "../components/Navbar";
 
 export default function Page() {
-
   
   return (
-      <div>
-      hola
-    </div>
-  );
-}
+    <div>
+     hola
+        </div>
+      )
+    }
+  
+
